1e69ba8f5af44fc36d62dddb406b3c1844f3298d
EthanPlail <ethanplail@Ethans-MBP.fios-router.home>
2017-04-06 14:36:35 -0400

    Update to Vac Program
    Changed the number of days a week from 7 to 5 reran set

--- Programs/VacationScrapper.py
+++ Programs/VacationScrapper.py
@@ -7,7 +7,7 @@ csvWriter = csv.writer(csvFile)
 CWD = os.getcwd()
 path = os.path.join(CWD,'WD')
 
-StartString = ['Name','First Amount','First Cut','Second Amount','Second Cut','Third Amount','Third Cut']
+StartString = ['WD','First Amount','First Cut','Second Amount','Second Cut','Third Amount','Third Cut']
 csvWriter.writerow(StartString)
 
 for dir,subdirs,files in os.walk(path):
@@ -18,17 +18,26 @@ for dir,subdirs,files in os.walk(path):
         innerPath = os.path.join(path,each)
         innerFile = open(innerPath, 'r+').read()
 
-        reGex = r"VACATION: (.+?) weeks paid vacation after (.+?) year of service"
-        reGex2 = r", (.+?) weeks after (.+?) years, and (.+?) weeks after (.+?) years."
-        match = re.finditer(reGex, innerFile)
-        match2 = re.finditer(reGex2, innerFile)
-        for item in match:
-            firstAmt = item.group(1)
-            firstCut = item.group(2)
-        for thing in match2:
-            secondAmt = thing.group(1)
-            secondCut = thing.group(2)
-            thirdAmt = thing.group(3)
-            thirdCut = thing.group(4)
+        startingPoint = False
+
+        startingPoint = re.search('ALL OCCUPATIONS LISTED ABOVE RECEIVE THE FOLLOWING BENEFITS:\n\nHEALTH & WELFARE:',innerFile)
+        endingPoint = re.search('HOLIDAYS:',innerFile)
+
+        if startingPoint:
+            f = innerFile[startingPoint.end():endingPoint.start()-2]
+
+            reGex = r"VACATION: (.+?) weeks paid vacation after (.+?) year"
+            reGex2 = r" (\d) weeks after (\d\d?) years, and (\d).+?after (\d\d?) years\."
+            match = re.finditer(reGex, f)
+            match2 = re.finditer(reGex2, f)
+            for item in match:
+                firstAmt = 5* int(item.group(1))
+                firstCut = item.group(2)
+            for thing in match2:
+                secondAmt = 5* int(thing.group(1))
+                secondCut = thing.group(2)
+                thirdAmt = 5* int(thing.group(3))
+                thirdCut = thing.group(4)
+
             VACinfo = [name,firstAmt,firstCut,secondAmt,secondCut,thirdAmt,thirdCut]
             csvWriter.writerow(VACinfo)
